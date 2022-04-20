597a18fefade7ab168f3a7cdc2e9eedd9cef4212
Sriharsha297 <sriharsha2098@gmail.com>
2022-04-21 04:00:50 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+
+  
+import React from 'react';
+import { withStyles, Typography } from '@material-ui/core';
+
+const styles = theme => ({
+    root: {
+        background: '#08002A',
+        color: '#fff',
+        padding: 2*theme.spacing.unit,
+    },
+
+    footer: {
+        width: '90%',
+        margin: 'auto',
+    },
+
+    text: {
+        color: '#ffffff',
+    }
+});
+
+const Footer = (props) => {
+
+    const { classes } = props;
+
+    return (
+        <div className={classes.root}>
+            <footer className={classes.footer}>
+                <Typography color="inherit" variant="subheading" >HCI Project</Typography>
+                
+            </footer>
+        </div>
+    );
+}
+
+export default withStyles(styles)(Footer);
